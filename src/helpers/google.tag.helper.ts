7bbd873804828831baf3ceac230d75bc37155fa6
Nevo David <nevo@notu.co>
2023-03-28 16:40:50 +0700

    feat: gtag event

--- src/helpers/google.tag.helper.ts
+++ src/helpers/google.tag.helper.ts
@@ -1,6 +1,7 @@
 export class GoogleTagHelper {
   static event(name: string, value: any) {
     if (!process.env.G_TAG_ANALYTICS || typeof window === "undefined") {
+      console.log("no gtag");
       return;
     }
 
