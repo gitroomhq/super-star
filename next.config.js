77da4c0a073041eb4887ea97beafacae7579098c
Nevo David <nevo@notu.co>
2023-03-28 14:27:00 +0700

    feat: google analytics

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     PAYMENT_PUBLIC_KEY: process.env.PAYMENT_PUBLIC_KEY,
+    G_TAG_ANALYTICS: process.env.G_TAG_ANALYTICS,
+    COURSE_NAME: process.env.COURSE_NAME,
   },
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@github20k/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@github20k/styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {!!process.env.G_TAG_ANALYTICS && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.G_TAG_ANALYTICS}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+    
+              gtag('config', '${process.env.G_TAG_ANALYTICS}');
+            `}
+          </Script>
+        </>
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
