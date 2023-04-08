d0d4f8e8df2c2100dfd01435867153937b5f7b82
Nevo David <nevo@notu.co>
2023-04-08 21:34:20 +0700

    feat: blog

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,9 +1,10 @@
 import { getGithubStars } from "@github20k/helpers/get.github.stars";
 import { BlogPostComponent } from "@github20k/components/blog/blog.post.component";
 import { blogService } from "@github20k/services/blog/blog.service";
+import {Details} from "@github20k/services/blog/blog.interface";
 
-export default function Index({ stargazers_count, blog }) {
-  return <BlogPostComponent stargazers_count={stargazers_count} blog={blog} />;
+export default function Index(props: { stargazers_count: number, blog: Details }) {
+  return <BlogPostComponent stargazers_count={props.stargazers_count} blog={props.blog} />;
 }
 
 export async function getStaticPaths() {
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,10 @@
 import { BlogComponent } from "@github20k/components/blog/blog.component";
 import { getGithubStars } from "@github20k/helpers/get.github.stars";
 import { blogService } from "@github20k/services/blog/blog.service";
+import {Details} from "@github20k/services/blog/blog.interface";
 
-export default function Index({ stargazers_count, blog }) {
-  return <BlogComponent stargazers_count={stargazers_count} blog={blog} />;
+export default function Index(props: { stargazers_count: number, blog: Details[] }) {
+  return <BlogComponent stargazers_count={props.stargazers_count} blog={props.blog} />;
 }
 
 export async function getStaticProps(context) {
