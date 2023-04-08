a60902d75be65e0dc1edd40611e5a4da9ce08d4d
Nevo David <nevo@notu.co>
2023-04-08 21:38:27 +0700

    feat: stars

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import HomeComponent from "@github20k/components/home/home.component";
 import { getGithubStars } from "@github20k/helpers/get.github.stars";
 
-export default function Home({ stargazers_count }) {
-  return <HomeComponent stargazers_count={stargazers_count} />;
+export default function Home(props: { stargazers_count: number }) {
+  return <HomeComponent stargazers_count={props.stargazers_count} />;
 }
 
 export async function getStaticProps(context) {
