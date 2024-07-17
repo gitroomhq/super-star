7e0d1771f111c8dba05eb30ed44b9d7dbd8832e0
Nevo David <nevo@notu.co>
2024-07-17 15:19:10 +0700

    feat: headers

--- next.config.mjs
+++ next.config.mjs
@@ -33,6 +33,20 @@ const nextConfig = {
     }
     return [];
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/success",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   rewrites() {
     if (process?.env?.BLOG_SERVICE === 'wordpress') {
       return [
