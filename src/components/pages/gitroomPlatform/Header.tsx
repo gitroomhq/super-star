b3116a1766868009fe5ccd56d84b06fc5dfdf9fc
Nevo David <nevo@notu.co>
2024-08-08 17:05:12 +0700

    feat: header

--- src/components/pages/gitroomPlatform/Header.tsx
+++ src/components/pages/gitroomPlatform/Header.tsx
@@ -135,7 +135,7 @@ const Header = () => {
             "lg:mt-6 lg:gap-[10px]"
           )}
         >
-          <Link href="https://gitroom.com/auth">
+          <Link href="https://platform,gitroom.com">
             <Button
               customClasses={clsx(
                 "w-[143px] h-[42px]",
